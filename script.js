3b12842ed0211f6a2e9a7276ebadaa029439c446
Michael Sam <sam.michael.h@gmail.com>
2024-03-02 12:55:02 +0900

    Change the document title to detect the tab is active or not. Center the h1 heading

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("visibilitychange", function() {
+  if(document.visibilityState === "visible") {
+    document.title = "Active Tab"
+  } else {
+    document.title = "Inactive Tab"
+  }
+})
